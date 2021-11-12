b2b280cb25fa169d6c1ef539ba9864e9a0989ddb
Kaio Brito <kaio.brito92@gmail.com>
2021-11-12 18:43:30 -0300

    Add schema tags to ModelViewSet

--- apis/bar/dummy/viewsets.py
+++ apis/bar/dummy/viewsets.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets
+from rest_framework.schemas.openapi import AutoSchema
 from .models import DummyBar
 from .serializers import DummySerializer
 
 class DummyViewSet(viewsets.ModelViewSet):
+    schema = AutoSchema(tags=['bar'])
     queryset = DummyBar.objects.all()
     serializer_class = DummySerializer
--- apis/foo/dummy/viewsets.py
+++ apis/foo/dummy/viewsets.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets
+from rest_framework.schemas.openapi import AutoSchema
 from .models import DummyBar
 from .serializers import DummySerializer
 
 class DummyViewSet(viewsets.ModelViewSet):
+    schema = AutoSchema(tags=['foo'])
     queryset = DummyBar.objects.all()
     serializer_class = DummySerializer
